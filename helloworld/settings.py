422205c143e5123e40a5c2f77ea19449dc4880cd
jamesg19 <48737452+jamesg19@users.noreply.github.com>
2022-01-04 02:51:46 -0500

    SOLUCIONANDO ERROR
    settings.DATA_UPLOAD_MAX_MEMORY_SIZE. DJANGO

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -15,7 +15,7 @@ import pathlib
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DATA_UPLOAD_MAX_MEMORY_SIZE = 49000000
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
